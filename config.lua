95ee8fe7817d701c35203ab31076b1847b0f1246
jujulaixd <143991446+jujulaixd@users.noreply.github.com>
2023-09-04 08:46:42 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,46 @@
+Config = {}
+Config.Sound = true
+Config.Framework = 'QBCore' -- 'ESX' or 'QBCore'
+Config.Locale = 'en'
+
+Config.CommandShow = {
+    command = 'show',
+    description = 'Open Dispatch',
+}
+
+Config.VehicleRob = {
+    command = 'vehrob',
+    description = 'Vehicle theft',
+}
+
+Config.CommandPanic = {
+    command = 'p',
+    description = 'Emergency button'
+}
+
+Config.CommandClear = {
+    command = 'cls',
+    description =  'Clear Alerts'
+}
+
+Config.Jobs = {
+    'police',
+    'ambulance'
+}
+
+Config.AllowedJobs = {
+    ["police"] = {
+        name = 'police',
+        label = 'LSPD',
+        command = 'alert',
+        descriptcommand = 'Send an alert to LSPD',
+        panic = true,
+    },
+    ["ambulance"] = {
+        name = 'ambulance',
+        label = 'EMS',
+        command = 'alertems',
+        descriptcommand = 'Send an alert to EMS',
+        panic = true,
+    },
+}
